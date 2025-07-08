9096e53d79a3d4af16320270ab48192f4281ec39
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2025-07-08 23:37:27 +0200

    Added Messenger queue implementation and integration tests

---
+++ src/bundle/Stamp/DeduplicateStamp.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\Messenger\Stamp;
+
+use Symfony\Component\Lock\Key;
+use Symfony\Component\Messenger\Exception\LogicException;
+use Symfony\Component\Messenger\Stamp\StampInterface;
+
+/**
+ * Backport of Symfony DeduplicateStamp.
+ */
+final class DeduplicateStamp implements StampInterface
+{
+    private Key $key;
+
+    private ?float $ttl;
+
+    private bool $onlyDeduplicateInQueue;
+
+    public function __construct(
+        string $key,
+        ?float $ttl = 300.0,
+        bool $onlyDeduplicateInQueue = false
+    ) {
+        if (!class_exists(Key::class)) {
+            throw new LogicException(\sprintf('You cannot use the "%s" as the Lock component is not installed. Try running "composer require symfony/lock".', self::class));
+        }
+
+        $this->key = new Key($key);
+        $this->ttl = $ttl;
+        $this->onlyDeduplicateInQueue = $onlyDeduplicateInQueue;
+    }
+
+    public function onlyDeduplicateInQueue(): bool
+    {
+        return $this->onlyDeduplicateInQueue;
+    }
+
+    public function getKey(): Key
+    {
+        return $this->key;
+    }
+
+    public function getTtl(): ?float
+    {
+        return $this->ttl;
+    }
+}
